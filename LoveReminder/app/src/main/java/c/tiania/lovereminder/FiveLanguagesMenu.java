f0d817de3a523806b34842e892dbf62652586b42
tianiachan <tiania.chan@gmail.com>
2020-09-20 13:59:18 -0700

    created intent

---
+++ LoveReminder/app/src/main/java/c/tiania/lovereminder/FiveLanguagesMenu.java
@@ -0,0 +1,14 @@
+package c.tiania.lovereminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FiveLanguagesMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_five_languages_menu);
+    }
+}
--- LoveReminder/app/src/main/java/c/tiania/lovereminder/MainActivity.java
+++ LoveReminder/app/src/main/java/c/tiania/lovereminder/MainActivity.java
@@ -1,14 +1,26 @@
 package c.tiania.lovereminder;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //method called when tap let's begin button
+    public void letsBeginPressed(View view)
+    {
+        //bind the two activites together using an intent
+        Intent intent = new Intent(this, FiveLanguagesActivity.class);
+        //start new activity to show fie languages menu
+        startActivity(intent);
     }
 }
